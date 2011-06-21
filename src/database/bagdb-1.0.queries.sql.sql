dc5bc5c5ce26ad78f4625267b1d7ea1b007df248
Milo van der Linden <milo@dogodigi.net>
2011-06-21 11:58:05 +0200

    qurey bestand verwijderd

--- src/database/bagdb-1.0.queries.sql.sql
+++
@@ -1,5 +0,0 @@
-drop table gemeente;
-create table gemeente as select gemeentecode,gemeentenaam,ST_Multi(ST_Union(woonplaats.geometrie)) as geometrie
-from gemeente_woonplaats, woonplaats where woonplaatscode = woonplaats.identificatie group by gemeentecode,gemeentenaam;
-alter table gemeente add column id bigserial;
-select probe_geometry_columns();
